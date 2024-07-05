40ea75d6b5a0a307b24515834affc81c71e7e7d0
Juan Rivera <jjrr0213@gmail.com>
2024-07-05 01:06:19 -0600

    Landing Page config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Ecommerce Page Proyect</h1>
+      <h1>Ecommerce Page Project</h1>
+      <h2>Landing Page Initialization</h2>
     </main>
   );
 }
