e782400fe89047e164cf6cba0ebf964eff961505
Juan Rivera <jjrr0213@gmail.com>
2024-07-24 22:01:26 -0600

    Actualizacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main>
       <h1>Ecommerce Page Project</h1>
       <h2>Landing Page Initialization</h2>
+      <h2>Actualizacion Ecommerce</h2>
     </main>
   );
 }
